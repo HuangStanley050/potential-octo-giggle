f07fbca998941a332b1c5cce85d89085fb6927f7
Stanley Huang <infamous_godhand@yahoo.com>
2020-07-01 19:25:12 +1000

    able to return a random chuck norris joke

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const axios = require("axios");
 const prefix = "%";
+
+const getChuckNorrisJoke = async (message) => {
+  let result = await axios.get(`https://api.chucknorris.io/jokes/random`);
+  console.log(result.data);
+  message.channel.send(result.data.value);
+};
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  if (message.content.startsWith(`${prfiex}rasengan`)) {
+  if (message.content.startsWith(`${prefix}rasengan`)) {
     message.channel.send("chidori");
+  } else if (message.content.startsWith(`${prefix}chuck norris`)) {
+    getChuckNorrisJoke(message);
   }
 });
 
